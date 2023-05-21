d3460e56347a5fb7c842df77b21d815816ecd3fe
zaelani <muhammadzaelanisap@gmail.com>
2023-05-21 19:40:41 +0700

    feat: setup navigation with gorouter

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'injector.dart';
+import 'presentation/blocs/core/bloc_observer.dart';
+import 'presentation/blocs/music/music_bloc.dart';
+
+import 'presentation/routes/router.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  initServices();
+
+  Bloc.observer = SimpleBlocObserver();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp(const MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MusicBloc>(
+          create: (context) => sl()..add(const MusicEvent.songFetched()),
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Gameco Music',
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
+        routeInformationProvider: router.routeInformationProvider,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
